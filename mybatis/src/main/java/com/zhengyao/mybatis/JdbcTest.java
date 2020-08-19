69d119bf9344d8a14d8f012c092087c25e2def49
zhengyao3 <zhengyao6@Foxmail.com>
2020-08-19 10:54:30 +0800

    增加注释-测试

--- mybatis/src/main/java/com/zhengyao/mybatis/JdbcTest.java
+++ mybatis/src/main/java/com/zhengyao/mybatis/JdbcTest.java
@@ -15,6 +15,7 @@ public class JdbcTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         //加载数据库驱动
         Class.forName("com.mysql.jdbc.Driver");
+        //ABCD
         //数据库连接
         String url = "jdbc:mysql://localhost:3306/learn_mybatis?characterEncoding=utf-8";
         String userName = "root";
--- mybatis/src/main/java/com/zhengyao/mybatis/test/MybatisTest.java
+++ mybatis/src/main/java/com/zhengyao/mybatis/test/MybatisTest.java
@@ -16,6 +16,7 @@ public class MybatisTest {
 
     @Test
     public void test1() throws IOException {
+        //CDEF
         //1.Resources工具类，配置文件的加载，把配置文件加载成字节输入流
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         //2.解析了配置文件，并创建了sqlSessionFactory工厂
